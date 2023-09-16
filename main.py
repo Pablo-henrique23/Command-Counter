764628ef1fe509084251049868d448e500eed7b4
Pablo Henrique <105394456+zecabum@users.noreply.github.com>
2023-09-16 03:12:43 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from pathlib import Path
 import func
 
+extensao = input("Selecione a linguagem a ser analisada: Python ou JS (py ou js)\n")
+while extensao != "py" and extensao != "js":
+    extensao = input('Digite apenas "py" ou "js": ')
+
 func.linha()
-print(f'Deseja analisar um projeto ou um unico arquivo? \nExemplo:\n"D:\Downloads\python\{""}bot\{""}" para um projeto\n"D:\Downloads\python\{""}bot\{""}funcoes.py" para um arquivo unico')
+print(f'Deseja analisar um projeto ou um unico arquivo? \nExemplo:\n"D:\Downloads\python\\bot\\" para um projeto\n"D:\Downloads\python\\bot\\funcoes.py" para um arquivo unico')
 func.linha()
 x = int(input('Digite 1 ou 2 respectivamente. '))
 if x == 1:
@@ -12,11 +16,11 @@ if x == 1:
     projeto = str(input("Digite o caminho do projeto: ")) # adquire o caminho do projeto
 
     total = func.criarDic(total=True)
-    arquivos = func.fila(arquivos,projeto) # cria a fila de arquivos a serem analisados
-    nomes = func.filaNome(nomes,projeto)   # mesma coisa que o de cima, mas sem o caminho, so o nome do arquivo
+    arquivos = func.fila(arquivos, projeto, extensao) # cria a fila de arquivos a serem analisados
+    nomes = func.filaNome(nomes, projeto, extensao)   # mesma coisa que o de cima, mas sem o caminho, so o nome do arquivo
     for arquivo in arquivos:               # arquivos = lista de arquivos .py em um projeto
         func.linha()
-        print(f'{arquivo}')
+        print(f'[+] {arquivo}')
         elementos = func.criarDic()
 
         try:
@@ -39,14 +43,14 @@ if x == 1:
         print(f'Arquivos analisados: {nomes_todos}\nCaminhos analisados: {caminhos}')
         func.linha()
     else:
-        print('Não houve Total, possivelmente há um erro de digitação no caminho dos arquivos.\nCaso esteja em alguma distro Linux, tente entrar na pasta e usar "." para se referir ao caminho dela.')
+        print('Erro! Projeto nao encontrado ou extensao errada. Possivelmente há um erro de digitação no caminho dos arquivos.\nCaso esteja em alguma distro Linux, tente entrar na pasta e usar "." para se referir ao caminho dela.')
     if len(erros) > 0:
         print(f"ARQUIVO(S) COM ERRO: {str(erros).replace('WindowsPath(','').replace(')','')}")
         func.linha()
 
 
 elif x == 2:
-    arquivo = Path(str(input('Digite o caminho do arquivo (com a extensão): ')))
+    arquivo = Path(str(input(f'Digite o caminho do arquivo (com {extensao}): ')))
     abrirarquivo = open(f'{arquivo}', 'r', encoding='utf8')
     elementos = func.logica(alvos=func.criarDic(), arquivo=abrirarquivo)
     abrirarquivo.close()
